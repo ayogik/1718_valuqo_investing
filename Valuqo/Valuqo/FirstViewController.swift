6cb05edda4da23ebd7428c82c9b2645fd27f5930
ayogik <aykoppol@gmail.com>
2018-01-23 19:18:34 -0500

    Location Sensing with Map and Second Page
    Updated application to detect user location and display it on a map, as
    well as have a second page where users could log in to their account
    and view their financial data

--- Valuqo/Valuqo/FirstViewController.swift
+++ Valuqo/Valuqo/FirstViewController.swift
@@ -1,25 +1,50 @@
 //
-//  FirstViewController.swift
+//  ViewController.swift
 //  Valuqo
 //
-//  Created by A K on 1/23/18.
-//  Copyright © 2018 A K. All rights reserved.
+//  Created by A K on 1/22/18.
+//  Copyright © 2018 Valuqo. All rights reserved.
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class FirstViewController: UIViewController {
-
+class ViewController: UIViewController , CLLocationManagerDelegate{
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager() //creates location manager object
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func locationGrab(_ sender: UIBarButtonItem) { //when authorized, find location to within 10m
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        mapView.showsUserLocation = true
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) { // update map
+        let userLocation:CLLocation = locations[0] as CLLocation
+        
+        manager.stopUpdatingLocation()
+        
+        let coordinations = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude,longitude: userLocation.coordinate.longitude)
+        let span = MKCoordinateSpanMake(0.2,0.2)
+        let region = MKCoordinateRegion(center: coordinations, span: span)
+        
+        mapView.setRegion(region, animated: true)
+    }
+    
 }
 
