79704ef1621f3ce8e3b3ea2aa919e260a816e1ca
Bill Zhao <bizhao@ctemc.org>
2018-06-03 17:39:28 -0400

    logo work
    good

--- server.js
+++ server.js
@@ -3,16 +3,24 @@ const favicon = require("express-favicon");
 const app = express()
 var path = require("path");
 
+
+app.use(express.static('views'));
+//app.use(express.static("public"));
+app.use(favicon(__dirname + "/views/public/vqlogo.png"));
+
 app.set('view engine','pug');
-app.set('views',__dirname + '/views');
+app.set('views', __dirname + '/views');
 
-app.use(express.static(__dirname + '/views'));
-app.use(favicon(__dirname + "/public/vqlogo.png"));
-
-app.get('/', function (req,res, next) {
+app.all('/*', function (req,res, next) {
   console.log(req.method)
   next()
 });
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send("Something broke!")
+  res.status(404).send ("404")
+})
 
 /*
 app.get('/index.html', function(req, res) {
