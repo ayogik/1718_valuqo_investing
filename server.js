80c06f05a021b1ffc6aa573e0e71717e310399f2
Bill Zhao <bizhao@ctemc.org>
2018-06-03 11:41:04 -0400

    pug support
    we got pug which is nice and now i kinda know how to load websites now
    which is nice

--- server.js
+++ server.js
@@ -3,13 +3,36 @@ const app = express()
 var path = require("path");
 
 app.set('view engine','pug');
-app.set('views','./pug');
+app.set('views',__dirname + '/views');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
+app.get('/', function (req,res, next) {
+  console.log(req.method)
+  next()
+});
+
+/*
 app.get('/index.html', function(req, res) {
   res.sendFile('index');
 });
+*/
+app.get('/', function(req, res) {
+  res.render('index')
+});
+app.get('/index.html', function(req, res) {
+  res.render('index', {title: "Dashboard"})
+});
+app.get('/charts.html', function(req, res) {
+  res.render('charts')
+});
+app.get('/login.html', function(req, res) {
+  res.render('login')
+});
+app.get('/register.html', function(req, res) {
+  res.render('login')
+});
+
 
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
