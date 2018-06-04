fab3334ed3029048126f30c373e09f1706ccbb07
Bill Zhao <bizhao@ctemc.org>
2018-06-04 15:24:33 -0400

    testing
    uhh

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const favicon = require("express-favicon");
-const app = express()
+const session = require("client-sessions");
+var mysql = require('mysql');
+const app = express();
 var path = require("path");
 
-
+app.enable('trust proxy');
 app.use(express.static('views'));
 //app.use(express.static("public"));
 app.use(favicon(__dirname + "/views/public/vqlogo.png"));
@@ -12,21 +14,64 @@ app.set('view engine','pug');
 app.set('views', __dirname + '/views');
 
 app.all('/*', function (req,res, next) {
-  console.log(req.method)
+  console.log(req.ip)
   next()
 });
 
+var connection = mysql.createConnection({
+  host     : 'valuqo.cf2muhtlwios.us-east-2.rds.amazonaws.com',
+  user     : 'bizhao@ctemc.org',
+  password : 'Valuqo12',
+  database : 'valuqo'
+});
+/* database stuff
+connection.connect();
+connection.query("NEW TABLE accounts",function (err, rows, fields) {
+  if (err) throw err
+})
+*/
 app.use(function (err, req, res, next) {
   console.error(err.stack)
   res.status(500).send("Something broke!")
   res.status(404).send ("404")
 })
 
-/*
-app.get('/index.html', function(req, res) {
-  res.sendFile('index');
+//sessions
+app.use(session({
+  cookieName: 'session',
+  secret: 'black-people',
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+}));
+
+app.get("/*" , function(req, res, next) {
+  if (req.path == "/login.html") {
+    next();
+  }
+  else {
+    if (req.session && req.session.user) { // Check if session exists
+      // lookup the user in the DB by pulling their email from the session
+      User.findOne({ email: req.session.user.email }, function (err, user) {
+        if (!user) {
+          // if the user isn't found in the DB, reset the session info and
+          // redirect the user to the login page
+          req.session.reset();
+          res.redirect('/login.html');
+        } else {
+          // expose the user to the template
+          res.locals.user = user;
+
+          // render the dashboard page
+          res.render('index', {title: "Valuqo - Dashboard"});
+        }
+      });
+    } else {
+      res.redirect('/login.html');
+    }
+  }
 });
-*/
+
+
 app.get('/', function(req, res) {
   res.render('index')
 });
@@ -40,7 +85,7 @@ app.get('/tables.html', function(req, res) {
   res.render('tables')
 });
 app.get('/login.html', function(req, res) {
-  res.render('login')
+  res.render('login', {title: "Valuqo - Login"})
 });
 app.get('/register.html', function(req, res) {
   res.render('login')
